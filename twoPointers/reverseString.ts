a3b1b6217b015b7afc1ccd64653d0dba93e504b8
azaz101hassan <azaz101hassan@gmail.com>
2024-09-21 23:44:39 +0500

    TS - 344. Reverse String

---
+++ twoPointers/reverseString.ts
@@ -0,0 +1,23 @@
+// 344. Reverse String
+
+/**
+ Do not return anything, modify s in-place instead.
+ */
+const reverseString = (s: string[]): string[] => {
+  let left = 0;
+  let right = s.length - 1;
+
+  while (left < right) {
+    const temp = s[left];
+    s[left] = s[right];
+    s[right] = temp;
+
+    left++;
+    right--;
+  }
+
+  return s;
+};
+
+console.log(reverseString(["h", "e", "l", "l", "o"]));
+console.log(reverseString(["H", "a", "n", "n", "a", "h"]));
